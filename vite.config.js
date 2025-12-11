30b2c39cf5e1165ea4d4ef6b819ee56873f91cb2
Cody Ivy <codyivy0@gmail.com>
2025-12-11 13:48:04 -0600

    Deploy attempt 3

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/c-ivy-static-pages-deploy/',
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
